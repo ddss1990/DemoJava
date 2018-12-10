4fbc9402f3ca0cabfd3d7957f853ec278728dd91
Vestel_DSS <1411356992@qq.com>
2018-12-10 11:30:39 +0800

    Ajax part5 Jackson

---
+++ web/src/com/dss/web/test/TestJackson.java
@@ -0,0 +1,24 @@
+package com.dss.web.test;
+
+import com.dss.web.bean.CartGoodsBean;
+import com.dss.web.bean.GoodsBean;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * FileName: TestJackson
+ * Author: Chris
+ * Date: 2018/12/10 10:08
+ * Description: Test Jackson
+ */
+public class TestJackson {
+    public static void main(String[] args) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        GoodsBean goodsBean = new GoodsBean("Java", 100D);
+        String goodsBeanJson = mapper.writeValueAsString(goodsBean);
+        System.out.println("goodsBeanJson = " + goodsBeanJson);
+        CartGoodsBean cartGoodsBean = new CartGoodsBean("java");
+        String cartGoodsBeanJson = mapper.writeValueAsString(cartGoodsBean);
+        System.out.println("cartGoodsBeanJson = " + cartGoodsBeanJson);
+    }
+}
